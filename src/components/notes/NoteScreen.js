6f5c1cd9bbea0022f5e11552c6fa808435cfdce7
SisifoDev <manuelperdomo1710@icloud.com>
2021-06-22 16:05:06 -0500

    HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,7 +5,21 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
-
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                ></textarea>
+                <div className="notes__image">
+                    <img src="https://image.freepik.com/vector-gratis/fondo-conexion-red-degradado_23-2148865392.jpg" alt="imagen" />
+                </div>
+            </div>
         </div>
     )
 }
